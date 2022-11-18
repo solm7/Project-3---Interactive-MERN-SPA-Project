b67da97e71a398ca79c09697c1d875477a989b73
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-18 12:06:52 -0500

    Home page text and styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,8 @@ function Home() {
   return (
     <p>
       Welcome to BragBoard the best place on the web to show off your gaming
-      achievements.
+      achievements. This website is for gamers to create their own BragBoards to
+      show their skills and stats to their friends!
       {/* Add more test about website and purpose */}
     </p>
   );
